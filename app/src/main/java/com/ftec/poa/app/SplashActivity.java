6e8989b336f6b2f31188d079b47b6389d8d944f2
joaovalves10 <48305521+joaovalves10@users.noreply.github.com>
2019-06-28 13:47:06 -0300

    Atualização 28/06/2019

--- app/src/main/java/com/ftec/poa/app/SplashActivity.java
+++ app/src/main/java/com/ftec/poa/app/SplashActivity.java
@@ -1,24 +1,46 @@
 package com.ftec.poa.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        SharedPreferences preferences =
+                getSharedPreferences("user_preferences", MODE_PRIVATE);
+        if (preferences.contains("ja_abriu_app")) {
+            mostrarLogin();
+        } else {
+            mostrarSplash();
+        }
+    }
+
+    private void mostrarSplash() {
+
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
-                startActivity(intent);
-                finish();
+                mostrarLogin();
             }
-        },1500);
+        }, 2000);
+    }
+
+    private void mostrarLogin() {
+        Intent intent = new Intent(SplashActivity.this,
+                LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    private void adicionarPreferenceJaAbriu(SharedPreferences preferences) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("ja_abriu_app", true);
+        editor.commit();
     }
 }
